00fb4cde1e74650fe9e8c539b81a59b03180b457
Steven Green <steven.greens10@gmail.com>
2017-05-16 15:53:12 -0400

    Added color

--- Culley/player.js
+++ Culley/player.js
@@ -1,6 +1,13 @@
-function Player(){
+function Player(playernumber){
     this.p; //specifies player
     this.s = round(random(col*row));
+    this.c;
+    
+    if(playernumber == 1){
+        this.c = color(255,0,0);
+    }else{
+        this.c = color(0,0,255);   
+    }
     
     this.start = function(){
         nodes[this.s].state = 1;
