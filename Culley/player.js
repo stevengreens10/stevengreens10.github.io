2a9f472763e671557494f9e64c4e64b8b8e42b00
Steven Green <steven.greens10@gmail.com>
2017-05-17 08:42:57 -0400

    Update player.js

--- Culley/player.js
+++ Culley/player.js
@@ -1,15 +1,10 @@
-function Player(playernumber){
+function Player(num){
     this.p; //specifies player
     this.s = round(random(col*row));
-    this.c;
-    
-    if(playernumber == 1){
-        this.c = color(255,0,0);
-    }else{
-        this.c = color(0,0,255);   
-    }
+    this.number = num;
     
     this.start = function(){
         nodes[this.s].state = 1;
+        nodes[this.s].owners.push(this.number);
     }
 }
